2b5633092fb64d0ec6089b9d6298a4ba32e809c2
KordianPiduch <kordianpiduch@outlook.com>
2022-06-05 20:16:55 +0200

    some changes during model searching

--- proj_heart_attack_cls/src/models/find_model.py
+++ proj_heart_attack_cls/src/models/find_model.py
@@ -2,9 +2,10 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.metrics import recall_score, precision_score, f1_score, accuracy_score
+from sklearn.metrics import RocCurveDisplay
 
 
-def check_model(model, x_train, x_test, y_train, y_test, rounds=2):
+def check_model(model, x_train, x_test, y_train, y_test, rounds=2, roc=False):
     model.fit(x_train, y_train)
 
     y_pred_train = model.predict(x_train)
@@ -15,3 +16,6 @@ def check_model(model, x_train, x_test, y_train, y_test, rounds=2):
     print("recall\t\t", np.round(recall_score(y_train,y_pred_train),rounds), "\t|", np.round(recall_score(y_test,y_pred_test),rounds))
     print("f1-score\t", np.round(f1_score(y_train,y_pred_train),rounds), "\t|", np.round(f1_score(y_test,y_pred_test),rounds))
     print("accuracy\t", np.round(accuracy_score(y_train,y_pred_train),rounds), "\t|", np.round(accuracy_score(y_test,y_pred_test),rounds))
+
+    if roc:
+        RocCurveDisplay.from_predictions(y_test, y_pred_test)
